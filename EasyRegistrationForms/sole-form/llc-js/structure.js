0234c4ff1f45c07e16f44901401a6b332b7a3d89
Francis2150 <franciskwadzoamuzu@gmail.com>
2025-11-04 15:57:00 +0000

    g

--- EasyRegistrationForms/sole-form/llc-js/structure.js
+++ EasyRegistrationForms/sole-form/llc-js/structure.js
@@ -1,10 +1,10 @@
 (function() {
-  // Structure module for Sole Proprietorship App
+  // Structure module for App
   const Structure = {
     // Exposed example function
     duplicateFieldset() {
       console.log("Duplicating fieldset...");
-      // You can call Structure.createOwnerBlock() manually if needed
+      // You can call Structure.createFieldBlock() manually if needed
     },
 
     anotherMethod() {
@@ -38,10 +38,8 @@
       <option value="British">British</option>
       <option value="American">American</option>
       <option value="Canadian">Canadian</option>
-     
       <option value="German">German</option>
       <option value="Chinese">Chinese</option>
-     
       <option value="South African">South African</option>
       <option value="Kenyan">Kenyan</option>
       <option value="Ivorian">Ivorian</option>
@@ -79,124 +77,91 @@
     }
 
     // ============================================================
-    // OWNER/PROPRIETOR SECTION (Simplified for Sole Proprietorship)
+    // BUSINESS INFORMATION SECTION
     // ============================================================
-    const ownerContainer = document.getElementById('iownerContainer');
-    const addOwnerBtn = document.getElementById('iaddOwnerBtn');
-
-    function createOwnerBlock(index) {
-      const fieldset = document.createElement('fieldset');
-      fieldset.id = `iowner${index}`;
-      fieldset.innerHTML = `
-        <legend>Proprietor ${index}</legend>
-        <div class="form-group"><label>Title:</label><select id="iowner${index}_title">${titleOptions}</select></div>
-        <div class="form-group"><label>First Name:</label><input id="iowner${index}_fname" type="text"></div>
-        <div class="form-group"><label>Middle Name:</label><input id="iowner${index}_mname" type="text"></div>
-        <div class="form-group"><label>Surname:</label><input id="iowner${index}_sname" type="text"></div>
-        <div class="form-group"><label>Former Name:</label><input id="iowner${index}_former" type="text"></div>
-        <div class="form-group"><label>Gender:</label><select id="iowner${index}_gender">${genderOptions}</select></div>
-        <div class="form-group"><label>Date of Birth:</label><input id="iowner${index}_dob" type="text"></div>
-        <div class="form-group"><label>Place of Birth:</label><input id="iowner${index}_pob" type="text"></div>
-        <div class="form-group"><label>Nationality:</label><input id="iowner${index}_nation" type="text"></div>
-       
-        <div class="form-group"><label>Occupation:</label><input id="iowner${index}_occupation" type="text"></div>
-        <div class="form-group"><label>Contact 1:</label><input id="iowner${index}_contact1" type="text"></div>
-        <div class="form-group"><label>Contact 2:</label><input id="iowner${index}_contact2" type="text"></div>
-        <div class="form-group"><label>Email:</label><input id="iowner${index}_email" type="email"></div>
-        <div class="form-group"><label>Mother's Name:</label><input id="iowner${index}_mother" type="text"></div>
-        <div class="form-group"><label>TIN:</label><input id="iowner${index}_tin" type="text"></div>
-        <div class="form-group"><label>Ghana Card:</label><input id="iowner${index}_ghanaCard" type="text"></div>
-
-        <h4>Residential Address</h4>
-        <div class="form-group"><label>GPS:</label><input id="iowner${index}_resGps" type="text"></div>
-        <div class="form-group"><label>House No.:</label><input id="iowner${index}_resHse" type="text"></div>
-        <div class="form-group"><label>Landmark:</label><input id="iowner${index}_resLandmark" type="text"></div>
-        <div class="form-group"><label>Street:</label><input id="iowner${index}_resStreet" type="text"></div>
-        <div class="form-group"><label>City:</label><input id="iowner${index}_resCity" type="text"></div>
-        <div class="form-group"><label>Town:</label><input id="iowner${index}_resTown" type="text"></div>
-        <div class="form-group"><label>District:</label><input id="iowner${index}_resDistrict" type="text"></div>
-        <div class="form-group"><label>Region:</label><input id="iowner${index}_resRegion" type="text"></div>
-        <div class="form-group"><label>Country:</label><input id="iowner${index}_resCountry" type="text"></div>
+    const businessInfoContainer = document.getElementById('businessInfoContainer');
+    
+    if (businessInfoContainer) {
+      businessInfoContainer.innerHTML = `
+        <fieldset id="businessInfo">
+          <legend>Business Information</legend>
+          <div class="form-group"><label>Business Name:</label><input id="businessName" type="text" class="cell"></div>
+          <div class="form-group"><label>Business Activities:</label><input id="businessActivities" type="text" class="cell"></div>
+          <div class="form-group"><label>Start Date:</label><input id="startDate" type="date" class="cell"></div>
+          <div class="form-group"><label>Estimated Annual Revenue:</label><input id="estimatedRevenue" type="number" class="cell"></div>
+          <div class="form-group"><label>Number of Employees:</label><input id="numEmployees" type="number" class="cell"></div>
+        </fieldset>
       `;
-
-      const removeBtn = makeRemoveButton('Remove Proprietor');
-      removeBtn.addEventListener('click', () => {
-        fieldset.remove();
-        renumberFieldsets('iownerContainer', 'iowner', 'Proprietor');
-      });
-      fieldset.appendChild(removeBtn);
-      
-      // Add class 'cell' to all input fields within this owner block
-      fieldset.querySelectorAll('input').forEach(input => input.classList.add('cell'));
-      
-      return fieldset;
-    }
-
-    // Initialize owner section if it exists
-    if (ownerContainer && addOwnerBtn) {
-      addOwnerBtn.addEventListener('click', () => {
-        const next = ownerContainer.querySelectorAll('fieldset').length + 1;
-        ownerContainer.appendChild(createOwnerBlock(next));
-        renumberFieldsets('iownerContainer', 'iowner', 'Proprietor');
-      });
-
-      if (!ownerContainer.querySelector('fieldset')) {
-        ownerContainer.appendChild(createOwnerBlock(1));
-      }
     }
 
     // ============================================================
     // BUSINESS LOCATION SECTION
     // ============================================================
-    const businessLocationFields = document.getElementById('ibusinessLocationFields');
-    if (businessLocationFields) {
-      businessLocationFields.innerHTML = `
-        <div class="form-group"><label>GPS Address:</label><input id="ibusinessGps" type="text"></div>
-        <div class="form-group"><label>Landmark:</label><input id="ibusinessLandmark" type="text"></div>
-        <div class="form-group"><label>Building No.:</label><input id="ibusinessBuildingNo" type="text"></div>
-        <div class="form-group"><label>Town:</label><input id="ibusinessTown" type="text"></div>
-        <div class="form-group"><label>Street Name:</label><input id="ibusinessStreetName" type="text"></div>
-        <div class="form-group"><label>City:</label><input id="ibusinessCity" type="text"></div>
-        <div class="form-group"><label>District:</label><input id="ibusinessDistrict" type="text"></div>
-        <div class="form-group"><label>Region:</label><input id="ibusinessRegion" type="text"></div>
-        <div class="form-group"><label>Postal Number:</label><input id="ibusinessPostalNumber" type="text"></div>
-        <div class="form-group"><label>Postal Town:</label><input id="ibusinessPostalTown" type="text"></div>
-        <div class="form-group"><label>Postal Region:</label><input id="ibusinessPostalRegion" type="text"></div>
-        <div class="form-group"><label>Business Contact:</label><input id="ibusinessContact" type="text"></div>
-        <div class="form-group"><label>Business Email:</label><input id="ibusinessEmail" type="email"></div>
+    const businessLocationContainer = document.getElementById('businessLocationContainer');
+    
+    if (businessLocationContainer) {
+      businessLocationContainer.innerHTML = `
+        <fieldset id="businessLocation">
+          <legend>Business Location</legend>
+          <div class="form-group"><label>GPS Address:</label><input id="gpsAddress" type="text" class="cell"></div>
+          <div class="form-group"><label>Landmark:</label><input id="landmark" type="text" class="cell"></div>
+          <div class="form-group"><label>Building No.:</label><input id="buildingNo" type="text" class="cell"></div>
+          <div class="form-group"><label>Town:</label><input id="town" type="text" class="cell"></div>
+          <div class="form-group"><label>Street Name:</label><input id="streetName" type="text" class="cell"></div>
+          <div class="form-group"><label>City:</label><input id="city" type="text" class="cell"></div>
+          <div class="form-group"><label>District:</label><input id="district" type="text" class="cell"></div>
+          <div class="form-group"><label>Region:</label><input id="region" type="text" class="cell"></div>
+          <div class="form-group"><label>Postal Number:</label><input id="postalNumber" type="text" class="cell"></div>
+          <div class="form-group"><label>Postal Town:</label><input id="postalTown" type="text" class="cell"></div>
+          <div class="form-group"><label>Postal Region:</label><input id="postalRegion" type="text" class="cell"></div>
+          <div class="form-group"><label>Business Contact:</label><input id="businessContact" type="text" class="cell"></div>
+          <div class="form-group"><label>Business Email:</label><input id="businessEmail" type="email" class="cell"></div>
+        </fieldset>
       `;
-      
-      // Add class 'cell' to all input fields
-      businessLocationFields.querySelectorAll('input').forEach(input => input.classList.add('cell'));
     }
 
     // ============================================================
-    // BUSINESS INFORMATION SECTION
+    // OWNER INFORMATION SECTION
     // ============================================================
-    const businessInfoFields = document.getElementById('ibusinessInfoFields');
-    if (businessInfoFields) {
-      businessInfoFields.innerHTML = `
-        <div class="form-group"><label>Business Name:</label><input id="ibusinessName" type="text"></div>
-        <div class="form-group"><label>Business Activities:</label><input id="ibusinessActivities" type="text"></div>
-        <div class="form-group"><label>Start Date:</label><input id="ibusinessStartDate" type="text"></div>
-        <div class="form-group"><label>Estimated Annual Revenue:</label><input id="ibusinessRevenue" type="number"></div>
-        <div class="form-group"><label>Number of Employees:</label><input id="ibusinessEmployees" type="number"></div>
+    const ownerInfoContainer = document.getElementById('ownerInfoContainer');
+    
+    if (ownerInfoContainer) {
+      ownerInfoContainer.innerHTML = `
+        <fieldset id="ownerInfo">
+          <legend>Owner Information</legend>
+          <div class="form-group"><label>Title:</label><select id="ownerTitle" class="cell">${titleOptions}</select></div>
+          <div class="form-group"><label>First Name:</label><input id="ownerFirstName" type="text" class="cell"></div>
+          <div class="form-group"><label>Middle Name:</label><input id="ownerMiddleName" type="text" class="cell"></div>
+          <div class="form-group"><label>Surname:</label><input id="ownerSurname" type="text" class="cell"></div>
+          <div class="form-group"><label>Former Name:</label><input id="ownerFormerName" type="text" class="cell"></div>
+          <div class="form-group"><label>Date of Birth:</label><input id="ownerDOB" type="date" class="cell"></div>
+          <div class="form-group"><label>Place of Birth:</label><input id="ownerPOB" type="text" class="cell"></div>
+          <div class="form-group"><label>Gender:</label><select id="ownerGender" class="cell">${genderOptions}</select></div>
+          <div class="form-group"><label>Nationality:</label><select id="ownerNationality" class="cell">${nationalityOptions}</select></div>
+          <div class="form-group"><label>Occupation:</label><input id="ownerOccupation" type="text" class="cell"></div>
+          <div class="form-group"><label>Contact 1:</label><input id="ownerContact1" type="text" class="cell"></div>
+          <div class="form-group"><label>Contact 2:</label><input id="ownerContact2" type="text" class="cell"></div>
+          <div class="form-group"><label>Email:</label><input id="ownerEmail" type="email" class="cell"></div>
+          <div class="form-group"><label>Mother's Name:</label><input id="motherName" type="text" class="cell"></div>
+          <div class="form-group"><label>TIN:</label><input id="ownerTIN" type="text" class="cell"></div>
+          <div class="form-group"><label>Ghana Card:</label><input id="ownerGhanaCard" type="text" class="cell"></div>
+          
+          <h4>Owner Residential Address</h4>
+          <div class="form-group"><label>GPS:</label><input id="ownerGPS" type="text" class="cell"></div>
+          <div class="form-group"><label>Landmark:</label><input id="ownerLandmark" type="text" class="cell"></div>
+          <div class="form-group"><label>House No.:</label><input id="ownerHouseNo" type="text" class="cell"></div>
+          <div class="form-group"><label>Street:</label><input id="ownerStreet" type="text" class="cell"></div>
+          <div class="form-group"><label>City:</label><input id="ownerCity" type="text" class="cell"></div>
+          <div class="form-group"><label>Town:</label><input id="ownerTown" type="text" class="cell"></div>
+          <div class="form-group"><label>District:</label><input id="ownerDistrict" type="text" class="cell"></div>
+          <div class="form-group"><label>Region:</label><input id="ownerRegion" type="text" class="cell"></div>
+          <div class="form-group"><label>Country:</label><input id="ownerCountry" type="text" class="cell"></div>
+        </fieldset>
       `;
-      
-      // Add class 'cell' to all input fields
-      businessInfoFields.querySelectorAll('input').forEach(input => input.classList.add('cell'));
-    }
-
-    // ============================================================
-    // Initialize all sections
-    // ============================================================
-    // Renumber all sections to ensure proper numbering
-    if (ownerContainer) {
-      renumberFieldsets('iownerContainer', 'iowner', 'Proprietor');
     }
 
     // Attach 'cell' class to all existing input fields on load
-    document.querySelectorAll('input').forEach(input => {
+    document.querySelectorAll('input, select').forEach(input => {
       input.classList.add('cell');
     });
   });
@@ -205,7 +170,7 @@
     const activeElement = document.activeElement;
 
     // Only work when the user is inside an input
-    if (activeElement && activeElement.tagName === 'INPUT') {
+    if (activeElement && (activeElement.tagName === 'INPUT' || activeElement.tagName === 'SELECT')) {
       event.preventDefault();
 
       // Get pasted text
@@ -217,8 +182,8 @@
         .split(/\t|\r?\n/)
         .map(v => v.trim());
 
-      // Get ALL visible, enabled inputs in document order
-      const inputs = Array.from(document.querySelectorAll('input:not([disabled]):not([hidden])'));
+      // Get ALL visible, enabled inputs and selects in document order
+      const inputs = Array.from(document.querySelectorAll('input:not([disabled]):not([hidden]), select:not([disabled]):not([hidden])'));
 
       // Find the index of the currently focused input
       const startIndex = inputs.indexOf(activeElement);
@@ -239,7 +204,10 @@
   });
 
   // Register module globally for cross-script access
-  if (typeof App !== 'undefined' && App.register) {
+  if (typeof App !== 'undefined') {
     App.register("Structure", Structure);
+  } else {
+    // Fallback if App is not defined
+    window.Structure = Structure;
   }
 })();
